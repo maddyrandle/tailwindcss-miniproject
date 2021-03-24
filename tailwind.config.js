3de0620d7ef774d373c0e7db85999dd388222e36
Madison Randle <madisonrandle25@gmail.com>
2021-03-24 01:15:19 -0500

    Define heading font
    - download google font and update tailwind extended theme
    - Insert new font theme to at the end of base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        headline: ['Oswald']
+      }
+    },
     debugScreens: {
       position: ['top', 'left'],
     },
